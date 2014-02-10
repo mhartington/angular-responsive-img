6d4980fbffceaf985a3c92151f86151d296ecc70
Sheniff <sheniff@gmail.com>
2014-02-09 17:31:42 -0800

    removing comments

--- responsive-img.js
+++ responsive-img.js
@@ -1,25 +1,3 @@
-/**
-@toc
-
-@param {Object} scope (attrs that must be defined on the scope (i.e. in the controller) - they can't just be defined in the partial html). REMEMBER: use snake-case when setting these on the partial!
-TODO
-
-@param {Object} attrs REMEMBER: use snake-case when setting these on the partial! i.e. my-attr='1' NOT myAttr='1'
-TODO
-
-@dependencies
-TODO
-
-@usage
-partial / html:
-TODO
-
-controller / js:
-TODO
-
-//end: usage
-*/
-
 'use strict';
 
 angular.module('brc.angular-responsive-img', [])
